976dca72e768ad99ef39de4471a140c4c7790895
kho903 <gmldnr2222@naver.com>
2021-12-02 14:08:46 +0900

    feat: 사용자 삭제를 위한 API 구현 - DELETE HTTP Method

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.user;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,13 @@ public class UserController {
                 .buildAndExpand(savedUser.getId())
                 .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null)
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
     }
 }
