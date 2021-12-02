f7bb04aea023a521068d13f268e959ed263fe4df
kho903 <gmldnr2222@naver.com>
2021-12-02 16:54:37 +0900

    feat: 프로그래밍으로 제어하는 Filtering 방법 - 개별 사용자 조회

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -11,7 +12,8 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-@JsonIgnoreProperties(value = {"password"})
+//@JsonIgnoreProperties(value = {"password"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
 
@@ -20,8 +22,6 @@ public class User {
     @Past
     private Date joinDate;
 
-//    @JsonIgnore
     private String password;
-//    @JsonIgnore
     private String ssn;
 }
