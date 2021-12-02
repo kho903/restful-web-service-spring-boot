ada194dfcc0b74c499c4818de7e4dc4192b5a107
kho903 <gmldnr2222@naver.com>
2021-12-02 19:24:21 +0900

    feat: Level3 단계의 REST API 구현을 위한 HATEOAS 적용

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -29,13 +34,18 @@ public class UserController {
 
     // GET /users/1 or /users/10 -> String
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public Resource<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null)
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
 
-        return user;
+        // HATEOAS
+        Resource<User> resource = new Resource<>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 
     @PostMapping("/users")
