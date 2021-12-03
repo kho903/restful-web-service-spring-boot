da5b7e9df41511a0be84e4e6aa221eac5e577b03
kho903 <gmldnr2222@naver.com>
2021-12-03 14:25:36 +0900

    feat: JPA를 이용한 사용자 추가와 삭제 - POST / DELETE HTTP Method

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -3,11 +3,18 @@ package com.example.restfulwebservice.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +35,7 @@ public class UserJpaController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public Resource<User> retrieveUser(@PathVariable int id) {
         Optional<User> user = userRepository.findById(id);
 
         if (!user.isPresent()) {
@@ -39,6 +46,22 @@ public class UserJpaController {
         ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         resource.add(linkTo.withRel("all-users"));
 
-        return user.get();
+        return resource;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-insert into user values(1, sysdate(), 'User1', 'test1111', '701010-1111111')
-insert into user values(2, sysdate(), 'User2', 'test2222', '801010-2222222')
-insert into user values(3, sysdate(), 'User3', 'test3333', '901010-1111111')
+insert into user values(99991, sysdate(), 'User1', 'test1111', '701010-1111111')
+insert into user values(99992, sysdate(), 'User2', 'test2222', '801010-2222222')
+insert into user values(99993, sysdate(), 'User3', 'test3333', '901010-1111111')
+-- JPA 특성상 1, 2, 3 순으로 아이디를 넣는데 JPA와 상관없이 들어간 임의로 만든
+-- 테스트 데이터가 1부터 시작하면 오류가 나므로 임의로 큰 값을 테스트로 넣음
